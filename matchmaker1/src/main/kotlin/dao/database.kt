2edf864eae20dcfa6f00291d8ad36ef54957edfe
v-makeev <curage6699@yandex.ru>
2018-12-09 23:51:54 +0300

    code cleanup

--- matchmaker1/src/main/kotlin/dao/database.kt
+++
@@ -1,9 +0,0 @@
-package dao
-
-import org.jetbrains.exposed.sql.Table
-
-object Users : Table("users") {
-    val password = text("password")
-    val login = text("login")
-    var score = integer("score")
-}
--- matchmaker1/src/main/kotlin/model/modelExt.kt
+++
@@ -1,6 +0,0 @@
-package model
-
-import dao.Users
-import org.jetbrains.exposed.sql.ResultRow
-
-internal fun ResultRow.toUser() = User(this[Users.login], this[Users.score], this[Users.password])
